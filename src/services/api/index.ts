104f2010b38dfe694c3b96380d854a8f8df007a5
Max Kim <hwaseen@gmail.com>
2021-10-29 03:47:05 +0900

    feature:Axios Modulize

---
+++ src/services/api/index.ts
@@ -0,0 +1,45 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import {
+  Semester,
+  Weather,
+  Corona,
+  Notification,
+  BackgroundImages,
+} from './types';
+
+const axiosClient: AxiosInstance = axios.create({
+  baseURL: process.env.BASE_URL,
+});
+
+class ApiClient {
+  static getSemester(): Promise<AxiosResponse<Semester>> {
+    return axiosClient.get('/semester');
+  }
+
+  static getWeather(): Promise<AxiosResponse<Weather[]>> {
+    return axiosClient.get('/weather');
+  }
+
+  static getCorona(): Promise<AxiosResponse<Corona>> {
+    return axiosClient.get('/corona');
+  }
+
+  static getNotification(): Promise<AxiosResponse<Notification[]>> {
+    return axiosClient.get('/notification');
+  }
+
+  static getBackground(params: {
+    campus: 'seoul' | 'global';
+  }): Promise<AxiosResponse<BackgroundImages>> {
+    if (params === undefined) {
+      throw Error('올바른 campus 파라미터를 넣어주세요');
+    }
+
+    const { campus } = params;
+    return axiosClient.get('/background', {
+      params: { campus },
+    });
+  }
+}
+
+export default ApiClient;
---
+++ src/services/api/types.ts
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase */
+
+type SemesterAct = '종강' | '개강';
+type SemesterKey = 'current' | 'next' | 'seasonal';
+type SemesterValue = {
+  act: SemesterAct;
+  due: string;
+  id: string;
+};
+
+export type Semester = Record<SemesterKey, SemesterValue>;
+
+export type Weather = {
+  afternoon_icon: string;
+  date: string;
+  id: number;
+  morning_icon: string;
+  regional: 'seoul' | 'global';
+  weatherId: number;
+};
+
+type CoronaDataPerCity = {
+  rate: number;
+  region: string;
+};
+
+type CoronaDataPerDate = {
+  rate: number;
+  day: string;
+};
+
+export type Corona = {
+  city: CoronaDataPerCity[];
+  total: CoronaDataPerDate[];
+  cityTimeStatus: string;
+};
+
+export type Notification = {
+  date: string;
+  id: number;
+  link: string;
+  title: string;
+};
+
+export type BackgroundImages = {
+  dayImageUrl: string;
+  nightImageUrl: string;
+};
