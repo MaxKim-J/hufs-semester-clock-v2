ec2fb4b2d766b0306d79d81cb370affd55f85b9b
Max Kim <hwaseen@gmail.com>
2021-11-03 20:12:11 +0900

    Feature:Initiate Sentry SDK for watching error

--- config/constants.cjs
+++ config/constants.cjs
@@ -3,11 +3,13 @@ const constants = {
     'process.env.ENV': "'development'",
     'process.env.BASE_URL':
       "'https://us-central1-hufssemesterclockfirebase.cloudfunctions.net'",
+    'process.env.SENTRY_DSN' : "'https://6358bd1d305f4f94a05b535e3844d957@o1059118.ingest.sentry.io/6047312'"
   },
   prod: {
     'process.env.ENV': "'production'",
     'process.env.BASE_URL':
       "'https://us-central1-hufssemesterclockfirebase.cloudfunctions.net'",
+    'process.env.SENTRY_DSN' : "'https://6358bd1d305f4f94a05b535e3844d957@o1059118.ingest.sentry.io/6047312'"
   },
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import App from './App';
+
+if (process.env.ENV === 'production') {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,6 +25,9 @@ const queryClient = new QueryClient({
 ReactDOM.render(
   <QueryClientProvider client={queryClient}>
     <App />
+    {process.env.ENV === 'development' ? (
+      <ReactQueryDevtools initialIsOpen={false} />
+    ) : null}
   </QueryClientProvider>,
   document.querySelector('#app')
 );
