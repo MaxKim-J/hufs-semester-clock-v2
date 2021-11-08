83de2889079ea9de83c6e3c6aa8b5dead8c1a3d2
Max Kim <hwaseen@gmail.com>
2021-11-08 16:56:44 +0900

    Setting:Remove unused module

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react';
+import addDays from 'date-fns/addDays';
 import CompA from './components/CompA';
 import AsyncBoundaryWithQuery from './components/Boundaries/AsyncBoundaryWithQuery';
 
@@ -6,6 +7,7 @@ function App(): ReactElement {
   return (
     <>
       <h1>외대 종강시계 V2</h1>
+      <div>{addDays(new Date(), 1).getTime()}</div>
       <AsyncBoundaryWithQuery>
         <CompA />
       </AsyncBoundaryWithQuery>
