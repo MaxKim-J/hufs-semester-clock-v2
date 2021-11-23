1fc79e1ddbd6a273fb2f38f3a12115db7debb8d5
Max Kim <hwaseen@gmail.com>
2021-11-23 23:48:37 +0900

    Setting: Complete Babel setting with corejs 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
+import { getItem } from '@/_shared/services/storage';
 
 function App(): ReactElement {
+  useEffect(() => {
+    b();
+  }, []);
+
+  const b = async () => {
+    const a = await getItem('dd');
+    return a;
+  };
+
   return (
     <>
       <h1>외대 종강시계 V2</h1>
