30673a3ba88586093f4bc282c19fc8fd324de1d9
Max Kim <hwaseen@gmail.com>
2021-12-18 14:46:00 +0900

    Feature: Change getBackgroundByTime util function logic

--- src/_shared/utils/getBackgroundByTime.ts
+++ src/_shared/utils/getBackgroundByTime.ts
@@ -1,6 +1,9 @@
 import { BackgroundImg } from '@shared/services/api/types';
 
-const getBackgroundByTime = (backgroundImage: BackgroundImg) =>
-  backgroundImage[new Date().getHours() < 18 ? 'dayImageUrl' : 'nightImageUrl'];
+const getBackgroundByTime = (backgroundImage: BackgroundImg) => {
+  const currentTime = new Date();
+  const isNightTime = currentTime.getHours() < 18 && currentTime.getHours() > 5;
+  return backgroundImage[isNightTime ? 'dayImageUrl' : 'nightImageUrl'];
+};
 
 export default getBackgroundByTime;
