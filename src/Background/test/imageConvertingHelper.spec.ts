263a152c6d9931f7e4e74a082e3648e0c79caca2
Max Kim <hwaseen@gmail.com>
2021-12-23 02:03:36 +0900

    Test: Change Background test cases with file mocking

--- src/Background/test/imageConvertingHelper.spec.ts
+++ src/Background/test/imageConvertingHelper.spec.ts
@@ -1,17 +1,19 @@
-import { convertImageToDataUrl } from '@/Background/utils/imageConvertingHelper';
+import { convertBlobToDataUrl } from '@/Background/utils/imageConvertingHelper';
 
-describe('convertImageToDataUrl: 이미지 URL을 blob으로 다운받아 DataUrl로 변환', () => {
-  it('jpeg 이미지 URL을 Base64 DataURL로 변환한다.', async () => {
-    const dataUrl = await convertImageToDataUrl(
-      'https://picsum.photos/536/354'
-    );
+describe('이미지 파일 blob을 DataUrl로 변환한다.', () => {
+  it('jpeg 이미지 파일을 Base64 DataURL로 변환한다.', async () => {
+    const jpegFile = new File(['mockFile'], 'mock.jpeg', {
+      type: 'image/jpeg',
+    });
+    const dataUrl = await convertBlobToDataUrl(jpegFile);
     expect(dataUrl).toMatch(/data:image\/jpeg;base64,.*/);
   });
 
-  it('png 이미지 URL을 Base64 DataURL로 변환한다.', async () => {
-    const dataUrl = await convertImageToDataUrl(
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/PNG_transparency_demonstration_1.png/280px-PNG_transparency_demonstration_1.png'
-    );
+  it('png 이미지 파일을 Base64 DataURL로 변환한다.', async () => {
+    const pngFile = new File(['mockFile'], 'mock.png', {
+      type: 'image/png',
+    });
+    const dataUrl = await convertBlobToDataUrl(pngFile);
     expect(dataUrl).toMatch(/data:image\/png;base64,.*/);
   });
 });
