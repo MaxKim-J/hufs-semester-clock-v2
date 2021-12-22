71504da8fafe799f736e9aac9aca433e551f1e79
Max Kim <hwaseen@gmail.com>
2021-12-22 22:29:45 +0900

    Test: Add TestBoundary component

--- src/Background/test/BackgroundContent.spec.tsx
+++ src/Background/test/BackgroundContent.spec.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { RecoilRoot } from 'recoil';
 import { waitFor, render } from '@testing-library/react';
 import { userBackgroundImage } from '@shared/atoms/userBackgroundImage';
+import { MutableSnapshot } from 'recoil';
+import TestBoundary from '@components/boundries/TestBoundary';
 import BackgroundContent from '@/Background/components/BackgroundContent';
 
 describe('배경화면 초기값 설정', () => {
-  const initializeState = ({ set }: any) => {
+  const recoilState = ({ set }: MutableSnapshot) => {
     set(userBackgroundImage, {
       status: 'initialized',
       value: {
@@ -17,25 +17,22 @@ describe('배경화면 초기값 설정', () => {
     });
   };
 
-  const queryClient = new QueryClient();
-
   it('배경화면 초기값이 Recoil에 설정되어 있을 경우 현재 시간에 따라 적합한 배경화면이 나타난다', async () => {
-    const { findByTestId } = await render(
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot initializeState={initializeState}>
-          <BackgroundContent />
-        </RecoilRoot>
-      </QueryClientProvider>
+    const { findByTestId } = render(
+      <TestBoundary recoilState={recoilState}>
+        <BackgroundContent />
+      </TestBoundary>
     );
 
     await waitFor(async () => {
       const backgroundElement = await findByTestId('backgroundImage');
-      const backgroundUrl =
+      const imageUrl =
         new Date().getHours() < 18 && new Date().getHours() > 5
           ? 'dayImageUrl'
           : 'nightImageUrl';
+
       expect(backgroundElement).toHaveStyle(
-        `background-image: url(${backgroundUrl})`
+        `background-image: url(${imageUrl})`
       );
     });
   });
---
+++ src/_shared/components/boundries/TestBoundary.tsx
@@ -0,0 +1,23 @@
+import { ReactChild } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { MutableSnapshot, RecoilRoot } from 'recoil';
+
+type TestRendererProps = {
+  children: ReactChild;
+  queryClient?: QueryClient;
+  recoilState?: (snapShot: MutableSnapshot) => void;
+};
+
+function TestBoundary({
+  queryClient = new QueryClient({}),
+  recoilState = () => {},
+  children,
+}: TestRendererProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot initializeState={recoilState}>{children}</RecoilRoot>
+    </QueryClientProvider>
+  );
+}
+
+export default TestBoundary;
