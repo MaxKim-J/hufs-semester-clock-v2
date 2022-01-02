fa36b70540bab8da2c5676aed0b39cd0019ca64f
Max Kim <hwaseen@gmail.com>
2022-01-03 04:16:35 +0900

    Chore: Change API baseUrl

--- constants.cjs
+++ constants.cjs
@@ -4,14 +4,14 @@ const constants = {
   dev: {
     'process.env.ENV': "'development'",
     'process.env.BASE_URL':
-      "'https://us-central1-hufssemesterclockfirebase.cloudfunctions.net'",
+      "'https://us-central1-hufssemesterclockfirebase.cloudfunctions.net/api'",
     'process.env.SENTRY_DSN' : "'https://6358bd1d305f4f94a05b535e3844d957@o1059118.ingest.sentry.io/6047312'",
     'process.env.VERSION': `'${version}'`
   },
   prod: {
     'process.env.ENV': "'production'",
     'process.env.BASE_URL':
-      "'https://us-central1-hufssemesterclockfirebase.cloudfunctions.net'",
+      "'https://us-central1-hufssemesterclockfirebase.cloudfunctions.net/api'",
     'process.env.SENTRY_DSN' : "'https://6358bd1d305f4f94a05b535e3844d957@o1059118.ingest.sentry.io/6047312'",
     'process.env.VERSION': `'${version}'`
   },
